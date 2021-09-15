86740f7bb17cdd430fe3d1342c937e9e4fe6cb0f
Ryan Schofield <ryanjschofield2@gmail.com>
2021-09-15 15:52:44 -0500

    Fix conways-game.py comments and practices

--- conways-game.py
+++ conways-game.py
@@ -1,14 +1,19 @@
 import os
 
-
 clear = lambda: os.system('clear')
 
 
-#create an empty list, append an empty list for every row (height) and append a
-# 0 for every column (width) to every empty sublist. Throughout this project,
-# for any element cells[i][j], i and j represent x and y coordinates
-# respectively on the grid.
 def init_empty_cells(width, height):
+    """
+    Create an empty list, append an empty list for every row (height) and append a
+    0 to every sublist for each column (width). For any element cells[i][j], 
+    i and j represent x and y coordinates respectively on the grid.
+
+    type width: int
+    type height: int
+    rtype: int[][]
+
+    """
     cells = []
     if width <= 0 or height <= 0:
         return(cells)
@@ -20,11 +25,16 @@ def init_empty_cells(width, height):
                 i.append(0)
         return(cells)
 
-#create an empty list. Append empty lists for every row (height) 
-# of parameter cells. add cells[i][j] to l[i][j] by iterating through
-# all i and j. Use two for-loops to do this. This method of iterating
-# through cells will occur frequently throughout this project.
+
 def copy(cells):
+    """
+    Create an empty list. Append empty lists for every row (height) 
+    of parameter cells. add cells[i][j] to l[i][j] by iterating through
+    all i and j.
+
+    type cells: int[][]
+    rtype: int[][]
+    """
     l = []
     for i in range(len(cells)):
         l.append([])
@@ -32,10 +42,18 @@ def copy(cells):
             l[i].append(cells[i][j])
     return(l)
 
-#check if a given 2D index is valid by using if statements, comparing the given
-# index to the width and height of parameter cells. If index is negative,
-# return False.
+
+
 def is_valid(row, column, cells):
+    """
+    Check if a given 2D index is valid, comparing the given
+    index to the width and height of parameter cells.
+
+    type row: int
+    type column: int
+    type cells: int[][]
+    rtype: bool
+    """
     if row < 0 or column < 0:
         return(False)
     else:
@@ -44,10 +62,16 @@ def is_valid(row, column, cells):
         else:
             return(False)
 
-#Use if statements to check if the element at given 2D index is 0 or 1,
-# and flip accordingly. Use the is_valid function to return -1 if given
-# index is invalid.
+
 def toggle_cell(row, column, cells):
+    """
+    Check if the element at given 2D index is 0 or 1, and flip accordingly.
+    Return -1 if given index is invalid.
+
+    type row: int
+    type column: int
+    type cells: int[][]
+    """
     isValid = is_valid(row, column, cells)
     if isValid:
         if cells[row][column] == 0:
@@ -59,12 +83,17 @@ def toggle_cell(row, column, cells):
         return(False)
 
 
-#check every combination of i-1, i, and i+1 with j-1, j, and j+1 (exclude
-# combination [i,j]), counting for "1"s using an initially empty counting
-# variable. Use if statements to do this. if is_valid returns False, return -1.
-# use modulus of height and width to check cells that are next to each other on
-# toroidal surface, even if they do not appear to be on 2D grid.
+ 
 def count_neighbors(cells, row, col):
+    """
+    Check every combination of i-1, i, and i+1 with j-1, j, and j+1 (exclude
+    combination [i,j]), using modulus to simulate torus, and count for "1"s.
+    Return -1 for invalid row or col.
+
+    type cells: int[][]
+    type row: int
+    type column: int
+    """
     isValid = is_valid(row, col, cells)
     count = 0
     if not isValid:
@@ -91,16 +120,18 @@ def count_neighbors(cells, row, col):
 
 
 
-#create an empty two dimensional list of appropriate size using
-# method above. append the counts of each element cells[i][j]
-# using the count function, with two for-loops similar to above.
-# apply the rules of the game using if statements, checking each
-# element of a copy of cells against corresponding element in the
-# 2D list of counts. change elements individually according to rules,
-# and return the updated copy.
+
 def update(cells):
+    """
+    Create an empty two dimensional list, append the counts of each element cells[i][j].
+    Apply the rules of the game, checking each element of cells against
+    corresponding element in the 2D list of counts. Return updated copy.
+
+    type cells: int[][]
+    rtype: int[][]
+    """
     x = []
-    c = copy(cells) # remember to make a copy
+    c = copy(cells) # this is the return copy
     for i in range(len(cells)):
         x.append([])
     for i in range(len(cells)):
@@ -117,9 +148,11 @@ def update(cells):
                 c[i][j] = 1
     return(c)
 
+
 def print_cells(cells):
     num_rows = len(cells)
     num_columns = len(cells[0])
+    # Print the cells to the console
     print(" ", end="")
     for i in range(num_columns):
         print("_", end="")
@@ -132,34 +165,37 @@ def print_cells(cells):
                 print("@", end="")
             else:
                 print(" ", end="")
-        print("|")
-        
+        print("|")    
     print(" ", end="")
     for i in range(num_columns):
         print("-", end="")
     print("")
 
 
-
-
 def get_coordinates(input_str):
+    """
+    Parse the user input and return a list of coordinate pairs.
+
+    type input_str: str
+    rtype: int[][]
+    """
     pairs = input_str.split(' ')
     for i in range(len(pairs)):
         pairs[i] = list(map(int, pairs[i].split(',')))
     return pairs
 
-#print_cells(init_empty_cells(5,5))
-#print(get_coordinates("52,17 13,12"))
-size = int(input("how big is the torus?\n"))
-new_cells = init_empty_cells(size, size)
-while True:
-    clear()
-    print_cells(new_cells)
-    user_input = input("Specify coordinates or press enter to update:\n")
-    if user_input == "":
-        new_cells = update(new_cells)
-    elif user_input == "quit":
-        break
-    else:
-        for pair in get_coordinates(user_input):
-            toggle_cell(pair[0] - 1, pair[1] - 1, new_cells)
+
+if __name__ == "__main__":
+    size = int(input("how big is the torus?\n"))
+    new_cells = init_empty_cells(size, size)
+    while True:
+        clear()
+        print_cells(new_cells)
+        user_input = input("Specify coordinates or press enter to update:\n")
+        if user_input == "":
+            new_cells = update(new_cells)
+        elif user_input == "quit":
+            break
+        else:
+            for pair in get_coordinates(user_input):
+                toggle_cell(pair[0] - 1, pair[1] - 1, new_cells)
